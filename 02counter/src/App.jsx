4fc3e60f29499edaba18bb58ead9965353bee535
Devansh Mundi <devanshmundi@gmail.com>
2024-02-10 02:56:19 +0530

    first commit

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+
+  // usestate is a hook to change the value in UI in all the locations
+  let [value,setvalue] = useState(0)
+
+  const addvalue = ()=>{
+    if(value<20)
+    {
+      value+=1;
+      setvalue(value);
+    }
+  }
+  const removevalue = ()=>{
+    if(value!=0)
+    {
+      value-=1;
+      setvalue(value);
+    }
+  }
+  return (
+    <>
+      <h1>hi devansh</h1>
+      <h1>Counter value: {value}</h1>
+
+      <button onClick={addvalue}>Add counter : {value}</button>
+      <br />      
+      <button onClick={removevalue}>Remove counter : {value}</button>      
+    </>
+  )
+}
+
+export default App
